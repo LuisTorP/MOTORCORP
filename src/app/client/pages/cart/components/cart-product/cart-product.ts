e50e2f81c22c90c46c1f0f923186a774956084d6
Renzo <renzofab2@hotmail.com>
2025-06-30 22:42:28 -0500

    feat: corrigiendo product

--- src/app/client/pages/cart/components/cart-product/cart-product.ts
+++ src/app/client/pages/cart/components/cart-product/cart-product.ts
@@ -8,23 +8,5 @@ import { Product } from '../../../products/interfaces/product.interface';
   styleUrl: './cart-product.scss',
 })
 export class CartProduct {
-  // @Input() product!: Product;
-  product: Partial<Product> = {
-    nombre: 'Motocicleta Honda XR150L',
-    portada:
-      'https://tesis-files.s3.us-east-2.amazonaws.com/motorcorp/212_1.png',
-    miniatura:
-      'https://tesis-files.s3.us-east-2.amazonaws.com/motorcorp/212_1.png',
-    marca: 'Honda',
-    descripcion:
-      'Motocicleta doble propósito, ideal para ciudad y caminos rurales.',
-    modelo: 'XR150L',
-    precio: 9500.0,
-    galeria: [
-      'https://example.com/img/honda-xr150l-1.jpg',
-      'https://example.com/img/honda-xr150l-2.jpg',
-    ],
-    tipo: 'moto',
-    stock: 9,
-  };
+  @Input() product!: Product;
 }
--- src/app/client/pages/products/pages/product-description/product-description.ts
+++ src/app/client/pages/products/pages/product-description/product-description.ts
@@ -10,6 +10,9 @@ import {
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../interfaces/product.interface';
 import { CardProduct } from '../../components/card-product/card-product';
+import { CartService } from '../../../cart/services/cart.service';
+import { APP_ROUTES } from '../../../../../routes.constant';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-description',
@@ -21,9 +24,12 @@ export class ProductDescription implements OnInit, OnChanges {
   @Input() productId!: string;
 
   private productService = inject(ProductService);
+  private cartService = inject(CartService);
+  private router = inject(Router);
 
   product = signal<Product | null>(null);
   randomProducts = this.productService.randomProducts;
+  routes = APP_ROUTES;
 
   ngOnInit(): void {
     this.getProduct(this.productId);
@@ -46,5 +52,12 @@ export class ProductDescription implements OnInit, OnChanges {
   async getProduct(id: string) {
     const product = await this.productService.getProductById(this.productId);
     this.product.set(product);
+  }
+
+  addProductToCart() {
+    const product = this.product();
+    if (!product) return;
+    this.cartService.addProduct(product);
+    this.router.navigateByUrl(this.routes.client.cart.cart);
   }
 }
